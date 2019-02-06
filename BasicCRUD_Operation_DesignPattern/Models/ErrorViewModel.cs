756c5807e2025b6268f16570decb162930ba9ada
shraktim <shahadathossainraktim@gmail.com>
2019-02-06 19:31:05 +0600

    project created

---
+++ BasicCRUD_Operation_DesignPattern/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BasicCRUD_Operation_DesignPattern.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
